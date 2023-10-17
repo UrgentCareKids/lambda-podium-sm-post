9f50eb44810db0280e22662bc6325b54d9f2f42e
mjconl6291 <mjconl6291@gmail.com>
2023-10-17 02:03:53 -0500

    refactored to have python create the csv

--- src/app.py
+++ src/app.py
@@ -2,6 +2,10 @@ import json
 import boto3
 import os
 import psycopg2
+import csv
+from datetime import datetime
+
+
 
 def handler():
     call_easebase()
@@ -21,10 +25,43 @@ def easebase_conn():
     return conn
 
 def call_easebase():
+    s3_bucket_name = '107635001951-us-east-2-prod-internal-facing-data'
+    s3_object_key = 'podium/' + 'SM_VISITS_' + datetime.now().strftime('%Y-%m-%d')
+    view = 'public.podium_sm_visits_dtl'
     _targetconnection = easebase_conn()
     cursor = _targetconnection.cursor()
-    key = os.environ['KEY']
-    secret = os.environ['SECRET']
-    proc_call = f"call public.podium_sm_gen_file({key}, {secret})"
-    cursor.execute(proc_call,)
-    _targetconnection.commit()
+    try:
+        cursor.execute(f'SELECT * FROM {view}')
+        colnames = [desc[0] for desc in cursor.description]
+        rows = cursor.fetchall()
+    except Exception as e:
+        print(f'Error executing SQL query: {e}')
+        _targetconnection.close()
+        exit(1)
+    # Define the CSV file path
+    csv_file_path = 'output.csv'
+
+    # Write data to a CSV file
+    with open(csv_file_path, 'w', newline='') as csv_file:
+        csv_writer = csv.writer(csv_file)
+        csv_writer.writerow(colnames)  # Write column headers
+        csv_writer.writerows(rows)
+
+    print(f'Data exported to {csv_file_path}')
+
+    # Upload the CSV file to S3
+    s3 = boto3.client(
+        's3',
+        aws_access_key_id=os.environ['KEY'],
+        aws_secret_access_key=os.environ['SECRET']
+    )
+
+    try:
+        s3.upload_file(csv_file_path, s3_bucket_name, s3_object_key)
+        print(f'CSV file uploaded to S3 bucket: s3://{s3_bucket_name}/{s3_object_key}')
+    except Exception as e:
+        print(f'Error uploading CSV file to S3: {e}')
+
+    # Close the database connection
+    cursor.close()
+    _targetconnection.close()
